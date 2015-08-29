4a9005f3455d936abcdaf9c4767d49b301883632
twanies <anthonydm@bubblepost.be>
2015-08-29 20:06:55 +0200

    WIP etcd publisher

---
+++ loadbalancer/etcd/publisher.go
@@ -0,0 +1,120 @@
+package etcd
+
+import (
+	"fmt"
+
+	"github.com/coreos/go-etcd/etcd"
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/loadbalancer"
+	"github.com/go-kit/kit/log"
+)
+
+type Publisher struct {
+	client    *etcd.Client
+	keyspace  string
+	factory   loadbalancer.Factory
+	logger    log.Logger
+	endpoints chan []endpoint.Endpoint
+	quit      chan struct{}
+}
+
+func NewPublisher(c *etcd.Client, k string, f loadbalancer.Factory, logger log.Logger) (*Publisher, error) {
+	logger = log.NewContext(logger).With("component", "ETCD Publisher")
+
+	p := &Publisher{
+		client:    c,
+		keyspace:  k,
+		factory:   f,
+		logger:    logger,
+		endpoints: make(chan []endpoint.Endpoint),
+		quit:      make(chan struct{}),
+	}
+
+	instances, err := p.getInstances()
+	if err != nil {
+		return nil, err
+	}
+	go p.loop(makeEndpoints(instances, f, logger))
+	return p, nil
+}
+
+func (p *Publisher) loop(endpoints []endpoint.Endpoint) {
+	endpointsChan := make(chan []endpoint.Endpoint)
+	go p.watchLoop(endpointsChan)
+
+	for {
+		select {
+		case p.endpoints <- endpoints:
+			fmt.Println("initialized endpoints")
+
+		case e := <-endpointsChan:
+			endpoints = e
+			fmt.Println("endpoints " + fmt.Sprintf("%d", len(endpoints)))
+
+		case <-p.quit:
+			return
+		}
+	}
+}
+
+func (p *Publisher) watchLoop(endpointsChan chan []endpoint.Endpoint) {
+	watchChan := make(chan *etcd.Response)
+	go p.client.Watch(p.keyspace, 0, true, watchChan, nil)
+
+	for {
+		select {
+		case <-watchChan:
+			instances, err := p.getInstances()
+			if err != nil {
+				p.logger.Log("err", err)
+				continue
+			}
+			endpointsChan <- makeEndpoints(instances, p.factory, p.logger)
+		}
+	}
+}
+
+func (p *Publisher) Endpoints() ([]endpoint.Endpoint, error) {
+	select {
+	case endpoints := <-p.endpoints:
+		return endpoints, nil
+	case <-p.quit:
+		return nil, loadbalancer.ErrPublisherStopped
+	}
+}
+
+func (p *Publisher) getInstances() ([]string, error) {
+	resp, err := p.client.Get(p.keyspace, false, true)
+	if err != nil {
+		return nil, err
+	}
+
+	instances := make([]string, len(resp.Node.Nodes))
+	for i, node := range resp.Node.Nodes {
+		n, err := p.client.Get(node.Key, false, true)
+		if err != nil {
+			return nil, err
+		}
+		instances[i] = n.Node.Value
+	}
+	p.logger.Log("endpoints fetched", fmt.Sprint(instances))
+	return instances, nil
+}
+
+func (p *Publisher) Stop() {
+	close(p.quit)
+}
+
+func makeEndpoints(addrs []string, f loadbalancer.Factory, logger log.Logger) []endpoint.Endpoint {
+	endpoints := make([]endpoint.Endpoint, 0, len(addrs))
+
+	for _, addr := range addrs {
+		endpoint, err := f(addr)
+		if err != nil {
+			logger.Log("instance", addr, "err", err)
+			continue
+		}
+		endpoints = append(endpoints, endpoint)
+	}
+	return endpoints
+}
---
+++ loadbalancer/etcd/publisher_test.go
@@ -0,0 +1,111 @@
+package etcd
+
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/coreos/go-etcd/etcd"
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/loadbalancer"
+	"github.com/go-kit/kit/log"
+	"golang.org/x/net/context"
+)
+
+func TestPublisher(t *testing.T) {
+	var (
+		logger = log.NewNopLogger()
+		e      = func(context.Context, interface{}) (interface{}, error) { return struct{}{}, nil }
+	)
+
+	factory := func(instance string) (endpoint.Endpoint, error) {
+		return e, nil
+	}
+
+	machines := []string{"http://127.0.0.1:2379"}
+	client := etcd.NewClient(machines)
+	p, err := NewPublisher(client, "/stream", factory, logger)
+	if err != nil {
+		t.Fatalf("failed to create new publisher: %v", err)
+	}
+	defer p.Stop()
+
+	if _, err := p.Endpoints(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBadFactory(t *testing.T) {
+	var (
+		logger = log.NewNopLogger()
+		e      = func(context.Context, interface{}) (interface{}, error) { return struct{}{}, nil }
+	)
+
+	factory := func(instance string) (endpoint.Endpoint, error) {
+		return e, errors.New("_")
+	}
+
+	machines := []string{"http://127.0.0.1:2379"}
+	client := etcd.NewClient(machines)
+	p, err := NewPublisher(client, "/stream", factory, logger)
+	if err != nil {
+		t.Fatalf("failed to create new publisher: %v", err)
+	}
+	defer p.Stop()
+
+	endpoints, err := p.Endpoints()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want, have := 0, len(endpoints); want != have {
+		t.Errorf("want %d, have %d", want, have)
+	}
+}
+
+func TestRefreshWithChange(t *testing.T) {
+	var (
+		logger = log.NewLogfmtLogger(os.Stderr)
+		e      = func(context.Context, interface{}) (interface{}, error) { return struct{}{}, nil }
+	)
+
+	factory := func(instance string) (endpoint.Endpoint, error) {
+		return e, nil
+	}
+
+	machines := []string{"http://127.0.0.1:2379"}
+	client := etcd.NewClient(machines)
+	p, err := NewPublisher(client, "/stream", factory, logger)
+	if err != nil {
+		t.Fatalf("failed to create new publisher: %v", err)
+	}
+	defer p.Stop()
+
+	p.Endpoints()
+
+	select {}
+}
+
+func TestErrPublisherStopped(t *testing.T) {
+	var (
+		logger = log.NewNopLogger()
+		e      = func(context.Context, interface{}) (interface{}, error) { return struct{}{}, nil }
+	)
+
+	factory := func(instance string) (endpoint.Endpoint, error) {
+		return e, errors.New("_")
+	}
+
+	machines := []string{"http://127.0.0.1:2379"}
+	client := etcd.NewClient(machines)
+	p, err := NewPublisher(client, "/stream", factory, logger)
+	if err != nil {
+		t.Fatalf("failed to create new publisher: %v", err)
+	}
+
+	p.Stop()
+
+	_, have := p.Endpoints()
+	if want := loadbalancer.ErrPublisherStopped; want != have {
+		t.Fatalf("want %v, have %v", want, have)
+	}
+}
